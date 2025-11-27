80c7e36b38db93386b4b3cc2986ef46f81f035fb
Rishan <rbanerjee@islstudent.ch>
2025-11-27 14:48:20 +0100

    Date: 27/11/2025 Lesson number: 5 1. Goal for today (C i)  My goal for today was to add all of the questions that I needed to add to finish my form, and to start saving the questions to a .json file. 2. Work completed (C iii)  I finished adding all of the questions, that I needed to add to my form, and I am also able to add all of the answers from the questions to a .json file. 3. Did you follow your plan? (C iii and C iv) Yes, I did follow my plan, as I completed everything that I wanted to complete for today. 4. Next step (C i)  In the next lesson, I want to add another page on my website, to view all of the data that is on the .json file.

--- main.py
+++ main.py
@@ -28,29 +28,53 @@ def submit():
     global name, email, age
     name = request.form['name']
     email = request.form['email']
+    phone_number = request.form['phone_number']
     date_of_birth = request.form['date_of_birth']
     country = request.form['country']
-    message = request.form['message']
     gender = request.form['gender']
+    location = request.form['location']
+    medical_needs = request.form['medical_needs']
+    other_needs = request.form['other_needs']
 
     session['name'] = name
     session['email'] = email
-    session['message'] = message
     session['date_of_birth'] = date_of_birth
     session['country'] = country
     session['gender'] = gender
+    session['phone_number'] = phone_number
+    session['location'] = location
+    session['medical_needs'] = medical_needs
+    session['other_needs'] = other_needs
 
-    if not name or not email or not date_of_birth or not country or not gender:
+    if not name or not email or not date_of_birth or not country or not gender or not phone_number or not location:
         flash('All fields are required!')
         return redirect(url_for('form'))
 
     session.pop('name', None)
     session.pop('email', None)
-    session.pop('message', None)
     session.pop('date_of_birth', None)
     session.pop('country', None)
+    session.pop('gender', None)
+    session.pop('phone_number', None)
+    session.pop('medical_needs', None)
+    session.pop('other_needs', None)
 
     flash('Thank you for registering!')
+
+    # Check if file exists
+    if os.path.exists('registrations.json'):
+        with open('registrations.json', 'r') as file:
+            data = json.load(file)
+    else:
+        data = []
+
+    # Add the new registration
+    data.append({'name': name, 'country': country, 'date_of_birth': date_of_birth, 'email': email, 'phone_number': phone_number, 'gender': gender, 'medical_needs': medical_needs, 'other_needs': other_needs})
+
+    # Save all registrations back to the file
+    with open('registrations.json', 'w') as file:
+        json.dump(data, file, indent=2)
+
     return redirect(url_for('form'))
 
 if __name__ == '__main__':
