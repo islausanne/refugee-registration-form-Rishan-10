fdb4361fd665bbe5e5fbc7fdf5c3b1ccdfd85313
Rishan <rbanerjee@islstudent.ch>
2025-12-02 12:37:23 +0100

    Date: 02/12/2025 Lesson number: 6 1. Goal for today (C i)  My goal for today was to make the view page and to start validating all of the inputs in my form. 2. Work completed (C iii)  I finished adding the view page, and I am able to display all of the information submitted on that page in a table, and I have a button to delete all of the data. I also validated all of my questions, and I changed my phone number to have a country code to make it easier to validate. 3. Did you follow your plan? (C iii and C iv) Yes, I did follow my plan, as I completed everything that I wanted to complete for today and more. 4. Next step (C i)  In the next lesson I want to work on cleaning up my code, by adding comments and fixing the indentation. I also want to add a picture onto the home page to fill up empty space.

--- main.py
+++ main.py
@@ -28,34 +28,43 @@ def submit():
     global name, email, age
     name = request.form['name']
     email = request.form['email']
-    phone_number = request.form['phone_number']
     date_of_birth = request.form['date_of_birth']
     country = request.form['country']
     gender = request.form['gender']
     location = request.form['location']
     medical_needs = request.form['medical_needs']
     other_needs = request.form['other_needs']
+    phone_number = request.form['phone']
+    phone_number_2 = request.form['phone2']
+    full_phone_number = f"{phone_number}{phone_number_2}"
 
     session['name'] = name
     session['email'] = email
     session['date_of_birth'] = date_of_birth
     session['country'] = country
     session['gender'] = gender
-    session['phone_number'] = phone_number
+    session['phone'] = phone_number
+    session['phone2'] = phone_number_2
     session['location'] = location
     session['medical_needs'] = medical_needs
     session['other_needs'] = other_needs
 
-    if not name or not email or not date_of_birth or not country or not gender or not phone_number or not location:
+    if not name or not email or not date_of_birth or not country or not gender or not phone_number or not phone_number_2 or not location:
         flash('All fields are required!')
+        return redirect(url_for('form'))
+
+    if len(full_phone_number) < 7 or len(full_phone_number) > 15:
+        flash('Phone number must be between 7 and 15 digits!')
         return redirect(url_for('form'))
 
     session.pop('name', None)
     session.pop('email', None)
     session.pop('date_of_birth', None)
     session.pop('country', None)
+    session.pop('location', None)
     session.pop('gender', None)
-    session.pop('phone_number', None)
+    session.pop('phone', None)
+    session.pop('phone2', None)
     session.pop('medical_needs', None)
     session.pop('other_needs', None)
 
@@ -69,13 +78,31 @@ def submit():
         data = []
 
     # Add the new registration
-    data.append({'name': name, 'country': country, 'date_of_birth': date_of_birth, 'email': email, 'phone_number': phone_number, 'gender': gender, 'medical_needs': medical_needs, 'other_needs': other_needs})
+    data.append({'name': name, 'country': country, 'date_of_birth': date_of_birth, 'email': email, 'phone_number': f"+{full_phone_number}", 'gender': gender, 'medical_needs': medical_needs, 'other_needs': other_needs})
 
     # Save all registrations back to the file
     with open('registrations.json', 'w') as file:
         json.dump(data, file, indent=2)
 
     return redirect(url_for('form'))
+
+@app.route('/view')
+def view_registrations():
+    if os.path.exists('registrations.json'):
+        with open('registrations.json', 'r') as file:
+            data = json.load(file)
+    else:
+        data = []
+    return render_template('view.html', registrations=data)
+
+@app.route('/delete')
+def delete_registration():
+    if os.path.exists('registrations.json'):
+        os.remove('registrations.json')
+        data=[]
+
+    flash('Registrations deleted successfully!')
+    return render_template('view.html', registrations=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
